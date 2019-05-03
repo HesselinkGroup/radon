4e0d35d7254a1cd68e69ebe2569190b84f474a13
Paul Hansen <paul.c.hansen@gmail.com>
2019-05-02 17:28:23 -0700

    renamed fan2para to map_fan2para and same for para2fan, for coordinate transformations

--- radon/fan2para.py
+++ radon/fan2para.py
@@ -2,13 +2,13 @@ import numpy as np
 
 # Coordinate transformation
 
-def fan2para(phi_fan, theta_src, d_src_iso):
+def map_fan2para(phi_fan, theta_src, d_src_iso):
     """
     Convert fan beam coordinates (phi, theta_src) to parallel beam coordinates (r, theta).
     """
     return -d_src_iso*np.sin(phi_fan), theta_src + phi_fan
 
-def para2fan(r, theta, d_src_iso):
+def map_para2fan(r, theta, d_src_iso):
     """
     Convert parallel beam coordinates (r, theta) to fan beam coordinates (phi, theta_src).
     """
@@ -16,4 +16,3 @@ def para2fan(r, theta, d_src_iso):
     return phi, theta - phi
 
 
-    
--- radon/tests/test_fan2para.py
+++ radon/tests/test_fan2para.py
@@ -1,16 +1,16 @@
 import numpy as np
-from radon.fan2para import fan2para, para2fan
+from radon.fan2para import map_fan2para, map_para2fan
 
 def test_fan2para():
     """Test fan2para transformation.
     
     Very simple by-hand verifiable tests.
     """
-    r, theta = fan2para(np.pi/4, 0.0, 1.0)
+    r, theta = map_fan2para(np.pi/4, 0.0, 1.0)
     np.testing.assert_allclose(r, -np.sqrt(2)/2)
     np.testing.assert_allclose(theta, np.pi/4)
 
-    r, theta = fan2para(0.0, -np.pi/4, 1.0)
+    r, theta = map_fan2para(0.0, -np.pi/4, 1.0)
     np.testing.assert_allclose(r, 0.0, atol=1e-9)
     np.testing.assert_allclose(theta, -np.pi/4)
     
@@ -19,11 +19,11 @@ def test_para2fan():
     
     Very simple by-hand verifiable tests.
     """
-    phi, theta_src = para2fan(1.0, 0.0, 1.0)
+    phi, theta_src = map_para2fan(1.0, 0.0, 1.0)
     np.testing.assert_allclose(phi, -np.pi/2)
     np.testing.assert_allclose(theta_src, np.pi/2, atol=1e-9)
     
-    phi, theta_src = para2fan(-1.0, np.pi/4, 1.0)
+    phi, theta_src = map_para2fan(-1.0, np.pi/4, 1.0)
     np.testing.assert_allclose(phi, np.pi/2)
     np.testing.assert_allclose(theta_src, -np.pi/4, atol=1e-9)
     
@@ -38,8 +38,8 @@ def test_fan_para_inversion():
     tt,pp = np.meshgrid(theta_src, phi_fan)
     for t,p in zip(tt.ravel(), pp.ravel()):
         d_src = 2.2 # arbitrary number
-        r, theta = fan2para(p, t, d_src)
-        p2, t2 = para2fan(r, theta, d_src)
+        r, theta = map_fan2para(p, t, d_src)
+        p2, t2 = map_para2fan(r, theta, d_src)
 
         np.testing.assert_allclose(p, p2, atol=1e-9)
         np.testing.assert_allclose(t, t2, atol=1e-9)
